063e0099d81c67d174abe0657ef8ef270cf99612
tammy vu <tammy.vsk@gmail.com>
2023-02-07 12:35:33 -0600

    start

--- Loops.java
+++ Loops.java
@@ -1,10 +1,40 @@
 public class Loops {
 
-  // static methods go here
+  public static void nHellos(int n) {
 
+    for (int i = n; i < n; i++){
+      if (i %10 == 1){
+        System.out.println(i + "st");
+      }else if (i  && 1 %10 == 2){
+        System.out.println(i + "nd");
+      }else if (1)
+    }
+
+    }
+  }
+
+  public static int nRandoms(int n) {
+    
+  }
+
+  public static boolean isPrime(int n) {
+    
+  }
+
+  public static String getFactors(int n){
+
+  }
+
+  public static void pepysSim() {
+    
+  }
+
+  public static void montyHall() {
+    
+  }
   public static void main(String[] args) {
 
-    // testing of static methods goes here
+  nHellos(null);
 
   }
 }
