0e5269f9cf3a771a6699642b9af4f5e4560f9d10
tammy <tammy.vsk@gmail.com>
2023-02-12 22:28:06 -0600

    suffixes added

--- Loops.java
+++ Loops.java
@@ -1,9 +1,15 @@
 public class Loops {
 
   public static void nHellos (int n){
-    for (i = 0; i < n; i++){
-      if( i%10 == 1){
-        Sysout  
+    for (int i = 1; i <= n; i++){
+      if( i%10 == 1 && i != 11 ){
+        System.out.println(i + "st hello");
+      }else if ( i%10 == 2 && i != 12){
+        System.out.println(i + "nd hello");
+      }else if ( i%10 == 3 && i != 13){
+        System.out.println(i + "rd hello");
+      }else{
+        System.out.println(i + "th hello");
       }
     }
 
@@ -12,7 +18,7 @@ public class Loops {
 
   public static void main(String[] args) {
     
-    nHellos(12)
+    nHellos(300);
 
   }
 }
