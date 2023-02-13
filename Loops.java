87ca07af9d03ec12fe016a585193693c985a91b6
tammy <tammy.vsk@gmail.com>
2023-02-12 22:01:36 -0600

    x

--- Loops.java
+++ Loops.java
@@ -1,10 +1,18 @@
 public class Loops {
 
-  // static methods go here
+  public static void nHellos (int n){
+    for (i = 0; i < n; i++){
+      if( i%10 == 1){
+        Sysout  
+      }
+    }
+
+  }
+
 
   public static void main(String[] args) {
-
-    // testing of static methods goes here
+    
+    nHellos(12)
 
   }
 }
